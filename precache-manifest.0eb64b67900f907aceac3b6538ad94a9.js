8cfa6197e0f765447337811f023c9188882db86a
arczhang <arczhang@tencent.com>
2019-09-01 11:20:22 +0800

    Updates

---
+++ precache-manifest.0eb64b67900f907aceac3b6538ad94a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34aecf89827ff7be8a2ab36dc42f7997",
+    "url": "/minigame/index.html"
+  },
+  {
+    "revision": "a5b9c2fb1b4b51520617",
+    "url": "/minigame/static/css/2.2d7ed268.chunk.css"
+  },
+  {
+    "revision": "3dfc83b9ed3766eadb2d",
+    "url": "/minigame/static/css/main.8c34c9de.chunk.css"
+  },
+  {
+    "revision": "a5b9c2fb1b4b51520617",
+    "url": "/minigame/static/js/2.1b9dd7b2.chunk.js"
+  },
+  {
+    "revision": "3dfc83b9ed3766eadb2d",
+    "url": "/minigame/static/js/main.a30d85c1.chunk.js"
+  },
+  {
+    "revision": "5747858602856c1c20db",
+    "url": "/minigame/static/js/runtime~main.b1ff0f94.js"
+  },
+  {
+    "revision": "a5169ac840f79fa2260a540dc5164d3c",
+    "url": "/minigame/static/media/header.a5169ac8.jpg"
+  }
+]);
